b5b79c070c9ea85a22364fd57499eee605b58448
JKGzenna <juanlu360@gmail.com>
2020-01-28 00:17:22 +0100

    update versions

--- 1.0/entrypoint.sh
+++ 1.0/entrypoint.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-#CREATE ENCRYPT KEY VARIABLE FOR OPENSSL DECRYPT-SCRIPT
-encrypt_key=$(cat /etc/secret-volume/encrypt_key)
-
-#DECRYT TAR OF SW_VERSION WITH OPENSSL DECRYPT-SCRIPT
-echo -n "$encrypt_key" | openssl enc -d -aes-256-cbc -in "/temporal/create/$SW_VERSION.encrypt" -out "/temporal/create/$SW_VERSION.tar" -pass stdin
-echo -n "$encrypt_key" | openssl enc -d -aes-256-cbc -in "/temporal/update/$SW_VERSION.encrypt" -out "/temporal/update/$SW_VERSION.tar" -pass stdin
-
-#UNTAR FILES OF SW_VERSION IN APP FOLDER
-tar -xvf /temporal/create/$SW_VERSION.tar -C /opt/clientesapp/create --strip 1
-tar -xvf /temporal/update/$SW_VERSION.tar -C /opt/clientesapp/update --strip 1
-
 #START CLIENTESAPP CREATE
 cd /opt/clientesapp/create
 sudo java -jar $SW_VERSION.jar
