835e8f578964ba601a17c2e916154f8dd532fb5d
JKGzenna <juanlu360@gmail.com>
2020-01-28 21:30:18 +0100

    update

--- 1.0/entrypoint.sh
+++ 1.0/entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-#START CLIENTESAPP CREATE
-cd /opt/clientesapp/create
-java -jar spring-boot-jpa-1.0.jar
 #RE-START CLIENTESAPP UPDATE
 cd /opt/clientesapp/update
 java -jar spring-boot-jpa-1.0.jar
