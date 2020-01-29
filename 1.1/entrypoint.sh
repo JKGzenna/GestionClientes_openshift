86ed704797a63b6fe6ffc1710c49d3e53d674965
JKGzenna <juanlu360@gmail.com>
2020-01-29 13:53:09 +0100

    Update entrypoint.sh

--- 1.1/entrypoint.sh
+++ 1.1/entrypoint.sh
@@ -5,6 +5,8 @@ encrypt_key=$(cat /etc/secret-volume/password)
 #UNZIP THE ENCRYPT AND DECRYT TO TAR OF SW_VERSION WITH OPENSSL DECRYPT-SCRIPT
 unzip /temporal/spring-boot-jpa-1.0.zip
 sleep 5
+rm -rf /temporal/spring-boot-jpa-1.0.zip
+sleep 5
 echo -n "$encrypt_key" | openssl enc -d -aes-256-cbc -in "/temporal/$SW_VERSION.encrypt" -out "/temporal/$SW_VERSION.tar" -pass stdin
 
 #UNTAR FILES OF SW_VERSION TO JAR AND COPY IN APP FOLDER
