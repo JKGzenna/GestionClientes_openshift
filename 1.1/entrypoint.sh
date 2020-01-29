7eea33c0fde882cdcb22f0f22cbf040517e0207d
JKGzenna <juanlu360@gmail.com>
2020-01-29 14:44:05 +0100

    Update entrypoint.sh

--- 1.1/entrypoint.sh
+++ 1.1/entrypoint.sh
@@ -3,13 +3,15 @@
 encrypt_key=$(cat /etc/secret-volume/password)
 
 #UNZIP THE ENCRYPT AND DECRYT TO TAR OF SW_VERSION WITH OPENSSL DECRYPT-SCRIPT
-unzip /procesado/$SW_VERSION.zip
-cp /procesado/$SW_VERSION.encrypt /temporal
-echo -n "$encrypt_key" | openssl enc -d -aes-256-cbc -in "/temporal/$SW_VERSION.encrypt" -out "/temporal/$SW_VERSION.tar" -pass stdin
+unzip /procesado/spring-boot-jpa-1.0.zip
+
+cp /procesado/spring-boot-jpa-1.0.encrypt /temporal
+
+echo -n "$encrypt_key" | openssl enc -d -aes-256-cbc -in "/temporal/spring-boot-jpa-1.0.encrypt" -out "/temporal/spring-boot-jpa-1.0.tar" -pass stdin
 
 #UNTAR FILES OF SW_VERSION TO JAR AND COPY IN APP FOLDER
-tar xvf /temporal/$SW_VERSION.tar -C /opt/clientesapp
+tar xvf /temporal/spring-boot-jpa-1.0.tar -C /opt/clientesapp
 
 #START CLIENTESAPP
 cd /opt/clientesapp
-java -jar $SW_VERSION.jar
+java -jar spring-boot-jpa-1.0.jar
