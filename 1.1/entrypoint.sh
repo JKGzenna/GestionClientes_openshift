08c2085d4218b2c32ea5832e5a7107c6fbde0eec
JKGzenna <juanlu360@gmail.com>
2020-01-29 02:50:39 +0100

    Update entrypoint.sh

--- 1.1/entrypoint.sh
+++ 1.1/entrypoint.sh
@@ -6,7 +6,7 @@ encrypt_key=$(cat /etc/secret-volume/password)
 echo -n "$encrypt_key" | openssl enc -d -aes-256-cbc -in "/temporal/$SW_VERSION.encrypt" -out "/temporal/$SW_VERSION.tar" -pass stdin
 
 #UNTAR FILES OF SW_VERSION IN APP FOLDER
-tar -xvf /temporal/$SW_VERSION.tar -C /opt/clientesapp --strip 1
+tar -xvf /temporal/$SW_VERSION.tar -C /opt/clientesapp
 
 #START CLIENTESAPP
 cd /opt/clientesapp
