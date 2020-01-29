c2df0de52421f51bd66188461f183d37a83f0757
JKGzenna <juanlu360@gmail.com>
2020-01-29 21:36:08 +0100

    update osh securized encrypt configuration
    update osh securized encrypt configuration

--- 1.1/entrypoint.sh
+++ 1.1/entrypoint.sh
@@ -4,10 +4,11 @@
 encrypt_key=$(cat /etc/secret-volume/password)
 
 ## UNTAR THE ENCRYPT SW_VERSION, COPY '.encrypt' FILE TO '/tmp' FOLDER,
-## DELETE '/temporal' FOLDER AND DECRYPT FILE TO '.tar' WITH OPENSSL
+## DELETE '/temporal' FILES AND DECRYPT FILE TO '.tar' WITH OPENSSL
 tar -xvf /temporal/$SW_VERSION.tar
 cp /temporal/$SW_VERSION.encrypt /tmp
 rm -rf /temporal/$SW_VERSION.tar
+rm -rf /temporal/$SW_VERSION.encrypt
 echo -n "$encrypt_key" | openssl enc -d -aes-256-cbc -in "/tmp/$SW_VERSION.encrypt" -out "/tmp/$SW_VERSION.tar" -pass stdin
 
 ## UNTAR FILE OF SW_VERSION TO '.jar', COPY '.jar' IN APP FOLDER
