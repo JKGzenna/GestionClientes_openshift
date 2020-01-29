3248260eca5564cf76a4aea091a6f26a88d3fd18
JKGzenna <juanlu360@gmail.com>
2020-01-29 06:31:08 +0100

    Update entrypoint.sh

--- 1.1/entrypoint.sh
+++ 1.1/entrypoint.sh
@@ -3,6 +3,7 @@
 encrypt_key=$(cat /etc/secret-volume/password)
 
 #DECRYT TO TAR OF SW_VERSION WITH OPENSSL DECRYPT-SCRIPT
+unzip /temporal/$SW_VERSION.zip
 echo -n "$encrypt_key" | openssl enc -d -aes-256-cbc -in "/temporal/$SW_VERSION.encrypt" -out "/temporal/$SW_VERSION.tar" -pass stdin
 
 #UNTAR FILES OF SW_VERSION TO JAR AND COPY IN APP FOLDER
