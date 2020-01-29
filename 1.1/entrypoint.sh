02f23e9c1001e807e4ee425fb860032401b9768e
JKGzenna <juanlu360@gmail.com>
2020-01-29 02:43:24 +0100

    Update entrypoint.sh

--- 1.1/entrypoint.sh
+++ 1.1/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #CREATE ENCRYPT KEY VARIABLE FOR OPENSSL DECRYPT-SCRIPT
-encrypt_key=$(cat /etc/secret-volume/encrypt_key)
+encrypt_key=$(cat /etc/secret-volume/password)
 
 #DECRYT TAR OF SW_VERSION WITH OPENSSL DECRYPT-SCRIPT
 echo -n "$encrypt_key" | openssl enc -d -aes-256-cbc -in "/temporal/$SW_VERSION.encrypt" -out "/temporal/$SW_VERSION.tar" -pass stdin
