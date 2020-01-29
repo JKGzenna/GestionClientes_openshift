d609fe6dee8b8d6e2162043564628d91e1d9b6c7
JKGzenna <juanlu360@gmail.com>
2020-01-29 17:47:05 +0100

    Update entrypoint.sh

--- 1.1/entrypoint.sh
+++ 1.1/entrypoint.sh
@@ -3,13 +3,13 @@
 #CREATE ENCRYPT KEY VARIABLE FOR OPENSSL DECRYPT-SCRIPT
 encrypt_key=$(cat /etc/secret-volume/password)
 
-#UNTAR THE ENCRYPT SW_VERSION, COPY TO TMP AND DECRYPT TO TAR WITH OPENSSL DECRYPT-SCRIPT
+#UNTAR THE ENCRYPT SW_VERSION, COPY ENCRYPT FILE TO TMP, DECRYPT TO TAR WITH OPENSSL DECRYPT-SCRIPT AND DELETE TEMPORAL FOLDER
 tar -xvf /temporal/$SW_VERSION.tar
 cp /temporal/$SW_VERSION.encrypt /tmp
-rm -rf /temporal/$SW_VERSION.encrypt
 echo -n "$encrypt_key" | openssl enc -d -aes-256-cbc -in "/tmp/$SW_VERSION.encrypt" -out "/tmp/$SW_VERSION.tar" -pass stdin
+rm -rf /temporal
 
-#UNTAR FILES OF SW_VERSION TO JAR, COPY JAR IN APP FOLDER AND ERASE ALL TEMP FILES
+#UNTAR FILES OF SW_VERSION TO JAR, COPY JAR IN APP FOLDER AND DELETE ALL TEMP FILES
 tar -xvf /tmp/$SW_VERSION.tar -C /opt/clientesapp
 rm -rf /tmp/$SW_VERSION.encrypt
 rm -rf /tmp/$SW_VERSION.tar
