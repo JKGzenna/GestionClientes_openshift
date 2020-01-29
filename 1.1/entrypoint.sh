a5947f0c682f21cb2820885eaf12a4d8cd90ef45
JKGzenna <juanlu360@gmail.com>
2020-01-29 03:43:48 +0100

    update

--- 1.1/entrypoint.sh
+++ 1.1/entrypoint.sh
@@ -3,7 +3,8 @@
 encrypt_key=$(cat /etc/secret-volume/password)
 
 #DECRYT TAR OF SW_VERSION WITH OPENSSL DECRYPT-SCRIPT
-echo -n "$encrypt_key" | openssl enc -d -aes-256-cbc -in "/temporal/$SW_VERSION.encrypt" -out "/opt/clientesapp/$SW_VERSION.tar" -pass stdin
+#echo -n "$encrypt_key" | openssl enc -d -aes-256-cbc -in "/temporal/$SW_VERSION.encrypt" -out "/opt/clientesapp/$SW_VERSION.jar" -pass stdin
+echo -n "$encrypt_key" | openssl enc -d -aes-256-cbc -in "/temporal/$SW_VERSION.encrypt" -out "/temporal/$SW_VERSION.tar" -pass stdin
 
 #UNTAR FILES OF SW_VERSION IN APP FOLDER
 tar -xvf /temporal/$SW_VERSION.tar -C /opt/clientesapp --strip 1
