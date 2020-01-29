01ba534ceb8758bc435750ff63aaec1b68f759b4
JKGzenna <juanlu360@gmail.com>
2020-01-29 15:36:27 +0100

    update securized config

--- 1.1/entrypoint.sh
+++ 1.1/entrypoint.sh
@@ -2,13 +2,13 @@
 #CREATE ENCRYPT KEY VARIABLE FOR OPENSSL DECRYPT-SCRIPT
 encrypt_key=$(cat /etc/secret-volume/password)
 
-#UNTAR THE ENCRYPT AND DECRYT TO TAR OF SW_VERSION WITH OPENSSL DECRYPT-SCRIPT
-tar xvf /procesado/spring-boot-jpa-1.0.tar -C /temporal
-echo -n "$encrypt_key" | openssl enc -d -aes-256-cbc -in "/temporal/spring-boot-jpa-1.0.encrypt" -out "/temporal/spring-boot-jpa-1.0.tar" -pass stdin
+#UNTAR THE ENCRYPT SW_VERSION, COPY TO TEMPORAL AND DECRYPT TO TAR WITH OPENSSL DECRYPT-SCRIPT
+tar xvf /temporal/$SW_VERSION.tar -C /temporal/procesado
+echo -n "$encrypt_key" | openssl enc -d -aes-256-cbc -in "/temporal/procesado/$SW_VERSION.encrypt" -out "/temporal/procesado/$SW_VERSION.tar" -pass stdin
 
-#UNTAR FILES OF SW_VERSION TO JAR AND COPY IN APP FOLDER
-tar xvf /temporal/spring-boot-jpa-1.0.tar -C /opt/clientesapp
+#UNTAR FILES OF SW_VERSION TO JAR AND COPY JAR IN APP FOLDER
+tar xvf /temporal/procesado/$SW_VERSION.tar -C /opt/clientesapp
 
-#START CLIENTESAPP
+#START CLIENTESAPP JAR
 cd /opt/clientesapp
-java -jar spring-boot-jpa-1.0.jar
+java -jar $SW_VERSION.jar
