85f9ab2592d8aee8570bcd3d9839d66791c33890
JKGzenna <juanlu360@gmail.com>
2020-01-29 13:51:34 +0100

    update config

--- 1.1/entrypoint.sh
+++ 1.1/entrypoint.sh
@@ -2,10 +2,9 @@
 #CREATE ENCRYPT KEY VARIABLE FOR OPENSSL DECRYPT-SCRIPT
 encrypt_key=$(cat /etc/secret-volume/password)
 
-#FIX PERMISSIONS
-chmod 775 /temporal/$SW_VERSION.encrypt && chown -R 10001 /temporal
-
 #UNZIP THE ENCRYPT AND DECRYT TO TAR OF SW_VERSION WITH OPENSSL DECRYPT-SCRIPT
+unzip /temporal/spring-boot-jpa-1.0.zip
+sleep 5
 echo -n "$encrypt_key" | openssl enc -d -aes-256-cbc -in "/temporal/$SW_VERSION.encrypt" -out "/temporal/$SW_VERSION.tar" -pass stdin
 
 #UNTAR FILES OF SW_VERSION TO JAR AND COPY IN APP FOLDER
