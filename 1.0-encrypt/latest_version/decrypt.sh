89cf03b183553f417d1ff82ae3f27399dcf46bbd
JKGzenna <juanlu360@gmail.com>
2020-01-31 20:12:19 +0100

    update osh securized encrypt configuration
    update osh securized encrypt configuration

---
+++ 1.0-encrypt/latest_version/decrypt.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#COLOCAR EL '.encrypt' EN LATEST VERSION O EN LA MISMA RUTA DEL SCRIPT Y EJECUTARLO DESDE ESE DIRECTORIO
+
+#CREAMOS LAS VARIABLES
+export encrypt_key="34836a8f11beeaf01e12cd296251c65f"
+export SW_VERSION="spring-boot-jpa-1.0"
+
+#HACEMOS LAS CONVERSIONES, DESENCRIPTAMOS Y BORRAMOS LOS TEMPORALES GENERADOS
+tar -xvf /$SW_VERSION.tar
+rm -rf $SW_VERSION.tar
+echo -n "$encrypt_key" | openssl enc -d -aes-256-cbc -in "$SW_VERSION.encrypt" -out "$SW_VERSION.tar" -pass stdin
+rm -rf $SW_VERSION.encrypt
+tar -xvf /tmp/$SW_VERSION.tar
+rm -rf $SW_VERSION.tar
+
+#MENSAJE Y SALIDA
+echo "Encriptación realizada con éxito"
+exit
