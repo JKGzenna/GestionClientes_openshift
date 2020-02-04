a0ea7ac16d102452973be652f3e80b214768a01f
JKGzenna <juanlu360@gmail.com>
2020-02-05 00:43:07 +0100

    update osh securized encrypt configuration
    update osh securized encrypt configuration

--- 1.0-encrypt/latest_version/create_updir/decrypt_update.sh
+++ 1.0-encrypt/latest_version/create_updir/decrypt_update.sh
@@ -6,13 +6,16 @@ encrypt_key=$(cat /etc/secret-volume/password)
 SW_VERSION="spring-boot-jpa-1.0"
 
 #HACEMOS LAS CONVERSIONES, DESENCRIPTAMOS Y BORRAMOS LOS TEMPORALES GENERADOS
-tar -xvf $SW_VERSION.tar
+tar -xf $SW_VERSION.tar
 rm -rf $SW_VERSION.tar
 echo -n "$encrypt_key" | openssl enc -d -aes-256-cbc -in "$SW_VERSION.encrypt" -out "$SW_VERSION.tar" -pass stdin
 rm -rf $SW_VERSION.encrypt
-tar -xvf $SW_VERSION.tar
+tar -xf $SW_VERSION.tar
 rm -rf $SW_VERSION.tar
+rm -rf /opt/clientesapp/SW_VERSION.jar
+cp $SW_VERSION.jar /opt/clientesapp
 
-#MENSAJE Y SALIDA
-echo "Desencriptación realizada con éxito"
-exit
+#MENSAJE Y EJECUACIÓN DEL UPDATE
+echo "Desencriptación realizada con éxito, actualizando y creando directorio '/uploads', recuerde entrar crear un cliente con imagen y crear el volumen para la carpeta 'uploads"
+sleep 5
+java -jar $SW_VERSION.jar
