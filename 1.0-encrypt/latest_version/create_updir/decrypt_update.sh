6ed3908b64059f7f2658532a384e29bac2212844
JKGzenna <juanlu360@gmail.com>
2020-02-05 02:33:35 +0100

    update osh securized encrypt configuration
    update osh securized encrypt configuration

--- 1.0-encrypt/latest_version/create_updir/decrypt_update.sh
+++ 1.0-encrypt/latest_version/create_updir/decrypt_update.sh
@@ -12,12 +12,8 @@ echo -n "$encrypt_key" | openssl enc -d -aes-256-cbc -in "$SW_VERSION.encrypt" -
 rm -rf $SW_VERSION.encrypt
 tar -xf $SW_VERSION.tar
 rm -rf $SW_VERSION.tar
-rm -rf /opt/clientesapp/SW_VERSION.jar
-cp $SW_VERSION.jar /opt/clientesapp
-rm -rf $SW_VERSION.jar
-cd /opt/clientesapp
 
 #MENSAJE Y EJECUACIÓN DEL UPDATE
 echo "Desencriptación realizada con éxito, desplegando actualización"
-sleep 10
+sleep 4
 java $SW_VERSION.jar
