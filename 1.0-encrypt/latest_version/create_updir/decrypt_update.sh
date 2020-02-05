7fb819c3309fdc331b86127dcb7cca14529db1cc
JKGzenna <juanlu360@gmail.com>
2020-02-05 03:20:01 +0100

    update osh securized encrypt configuration
    update osh securized encrypt configuration

--- 1.0-encrypt/latest_version/create_updir/decrypt_update.sh
+++ 1.0-encrypt/latest_version/create_updir/decrypt_update.sh
@@ -6,14 +6,16 @@ encrypt_key=$(cat /etc/secret-volume/password)
 SW_VERSION="spring-boot-jpa-1.0"
 
 #HACEMOS LAS CONVERSIONES, DESENCRIPTAMOS Y BORRAMOS LOS TEMPORALES GENERADOS
-tar -xf $SW_VERSION.tar
-rm -rf $SW_VERSION.tar
-echo -n "$encrypt_key" | openssl enc -d -aes-256-cbc -in "$SW_VERSION.encrypt" -out "$SW_VERSION.tar" -pass stdin
-rm -rf $SW_VERSION.encrypt
-tar -xf $SW_VERSION.tar
-rm -rf $SW_VERSION.tar
+tar -xf /opt/clientesapp/update/$SW_VERSION.tar
+rm -rf /opt/clientesapp/update/$SW_VERSION.tar
+echo -n "$encrypt_key" | openssl enc -d -aes-256-cbc -in "/opt/clientesapp/update/$SW_VERSION.encrypt" -out "/opt/clientesapp/update/$SW_VERSION.tar" -pass stdin
+rm -rf /opt/clientesapp/update/$SW_VERSION.encrypt
+tar -xf /opt/clientesapp/update/$SW_VERSION.tar
+rm -rf /opt/clientesapp/update/$SW_VERSION.tar
 rm -rf /opt/clientesapp/$SW_VERSION.jar
 cp /opt/clientesapp/update/$SW_VERSION.jar /opt/clientesapp
-#MENSAJE
-echo "Desencriptación realizada con éxito, ejecute 'java -jar /opt/clientesapp/$SW_VERSION.jar' para terminar de aplicar la actualización"
-exit
+
+#MENSAJE Y EJECUCIÓN
+echo "Desencriptación realizada con éxito, ejecutando update de $SW_VERSION.jar' para terminar de aplicar la actualización"
+sleep 8
+java -jar /opt/clientesapp/$SW_VERSION.jar
