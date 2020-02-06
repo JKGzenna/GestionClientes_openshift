44797bee9a917e2195cca32e9bcc54864e643fa8
JKGzenna <juanlu360@gmail.com>
2020-02-06 03:45:47 +0100

    update osh securized encrypt configuration
    update osh securized encrypt configuration

--- 1.0-encrypt/entrypoint.sh
+++ 1.0-encrypt/entrypoint.sh
@@ -5,17 +5,16 @@ encrypt_key=$(cat /etc/secret-volume/password)
 
 ## UNTAR THE ENCRYPT SW_VERSION, COPY '.encrypt' FILE TO '/tmp' FOLDER,
 ## DELETE '/temporal' FILES AND DECRYPT FILE TO '.tar' WITH OPENSSL
-tar -xvf /temporal/$SW_VERSION.tar
-cp /temporal/$SW_VERSION.encrypt /tmp
-rm -rf /temporal/$SW_VERSION.tar
-rm -rf /temporal/$SW_VERSION.encrypt
+tar -xf /tmp/$SW_VERSION.tar
+rm -rf /tmp/$SW_VERSION.tar
 echo -n "$encrypt_key" | openssl enc -d -aes-256-cbc -in "/tmp/$SW_VERSION.encrypt" -out "/tmp/$SW_VERSION.tar" -pass stdin
 
 ## UNTAR FILE OF SW_VERSION TO '.jar', COPY '.jar' IN APP FOLDER
 ## AND DELETE RESIDUAL FILES '.encrypt' AND '-tar' ON '/temp' FOLDER
-tar -xvf /tmp/$SW_VERSION.tar -C /opt/clientesapp
+tar -xf /tmp/$SW_VERSION.tar -C /opt/clientesapp
 rm -rf /tmp/$SW_VERSION.encrypt
 rm -rf /tmp/$SW_VERSION.tar
 
 ## START CLIENTESAPP '.jar'
-java -jar /opt/clientesapp/$SW_VERSION.jar
+cd /opt/clientesapp
+java -jar $SW_VERSION.jar
