b9d89c9890dc1edda5d12c84e5599c35ba7184ca
JKGzenna <juanlu360@gmail.com>
2020-02-06 04:24:31 +0100

    update osh securized encrypt configuration
    update osh securized encrypt configuration

--- 1.0-encrypt/entrypoint.sh
+++ 1.0-encrypt/entrypoint.sh
@@ -5,15 +5,21 @@ encrypt_key=$(cat /etc/secret-volume/password)
 
 ## UNTAR THE ENCRYPT SW_VERSION, DELETE '.tar'
 ## AND DECRYPT FILE TO '.tar' WITH OPENSSL
-tar -xf /tmp/$SW_VERSION.tar
+cd /tmp
+tar -xf $SW_VERSION.tar
 rm -rf /tmp/$SW_VERSION.tar
 echo -n "$encrypt_key" | openssl enc -d -aes-256-cbc -in "/tmp/$SW_VERSION.encrypt" -out "/tmp/$SW_VERSION.tar" -pass stdin
 
 ## UNTAR FILE OF SW_VERSION TO '.jar', COPY '.jar' IN '/opt/clientesapp'
 ## FOLDER AND DELETE '.encrypt' AND '-tar' FILES AT '/temp' FOLDER
-tar -xf /tmp/$SW_VERSION.tar -C /opt/clientesapp
+cd /tmp
+tar -xf $SW_VERSION.tar -C /opt/clientesapp
 rm -rf /tmp/$SW_VERSION.encrypt
 rm -rf /tmp/$SW_VERSION.tar
+
+## SUCCESS MESSAGE
+echo "Desencriptación realizada con éxito, ejecutando $SW_VERSION.jar"
+sleep 3
 
 ## GO TO '/opt/clienteapp' FOLDER AND START '.jar'
 cd /opt/clientesapp
