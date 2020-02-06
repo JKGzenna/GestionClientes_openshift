1148656e74a0b43903027d2709363fa7dbe0a3f3
JKGzenna <juanlu360@gmail.com>
2020-02-06 04:22:37 +0100

    update osh securized encrypt configuration
    update osh securized encrypt configuration

--- 1.0-encrypt/entrypoint.sh
+++ 1.0-encrypt/entrypoint.sh
@@ -3,19 +3,24 @@
 ## CREATE ENCRYPT KEY VARIABLE FOR OPENSSL DECRYPT-SCRIPT
 encrypt_key=$(cat /etc/secret-volume/password)
 
-## UNTAR THE ENCRYPT SW_VERSION, COPY '.encrypt' FILE TO '/tmp' FOLDER,
-## DELETE '/temporal' FILES AND DECRYPT FILE TO '.tar' WITH OPENSSL
-tar -xf /tmp/$SW_VERSION.tar
+## UNTAR THE ENCRYPT SW_VERSION, DELETE '.tar'
+## AND DECRYPT FILE TO '.tar' WITH OPENSSL
+cd /tmp
+tar -xf $SW_VERSION.tar
 rm -rf /tmp/$SW_VERSION.tar
 echo -n "$encrypt_key" | openssl enc -d -aes-256-cbc -in "/tmp/$SW_VERSION.encrypt" -out "/tmp/$SW_VERSION.tar" -pass stdin
-rm -rf /tmp/$SW_VERSION.encrypt
 
-## UNTAR FILE OF SW_VERSION TO '.jar', COPY '.jar' IN APP FOLDER
-## AND DELETE RESIDUAL FILES '.encrypt' AND '-tar' ON '/temp' FOLDER
-tar -xf /tmp/$SW_VERSION.tar -C /opt/clientesapp
+## UNTAR FILE OF SW_VERSION TO '.jar', COPY '.jar' IN '/opt/clientesapp'
+## FOLDER AND DELETE '.encrypt' AND '-tar' FILES AT '/temp' FOLDER
+cd /tmp
+tar -xf $SW_VERSION.tar -C /opt/clientesapp
 rm -rf /tmp/$SW_VERSION.encrypt
 rm -rf /tmp/$SW_VERSION.tar
 
-## START CLIENTESAPP '.jar'
+## SUCCESS MESSAGE
+echo "Desencriptación realizada con éxito, ejecutando $SW_VERSION.jar"
+sleep 3
+
+## GO TO '/opt/clienteapp' FOLDER AND START '.jar'
 cd /opt/clientesapp
 java -jar $SW_VERSION.jar
