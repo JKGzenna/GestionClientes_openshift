a835db48ba32b37284e213476a689c7656b1d8ab
JKGzenna <juanlu360@gmail.com>
2020-02-06 05:39:49 +0100

    update osh securized encrypt configuration
    update osh securized encrypt configuration

--- 1.0-encrypt/entrypoint.sh
+++ 1.0-encrypt/entrypoint.sh
@@ -2,20 +2,31 @@
 
 ## CREATE ENCRYPT KEY VARIABLE FOR OPENSSL DECRYPT-SCRIPT
 encrypt_key=$(cat /etc/secret-volume/password)
+sleep 2
 
 ## UNTAR THE ENCRYPT SW_VERSION, DELETE '.tar'
 ## AND DECRYPT FILE TO '.tar' WITH OPENSSL
 cd /tmp
 tar -xf $SW_VERSION.tar
+sleep 4
+cd /tmp
 rm -rf /tmp/$SW_VERSION.tar
-echo -n "$encrypt_key" | openssl enc -d -aes-256-cbc -in "/tmp/$SW_VERSION.encrypt" -out "/tmp/$SW_VERSION.tar" -pass stdin
+sleep 1
+cd /tmp
+echo -n "$encrypt_key" | openssl enc -d -aes-256-cbc -in "$SW_VERSION.encrypt" -out "$SW_VERSION.tar" -pass stdin
+sleep 4
 
 ## UNTAR FILE OF SW_VERSION TO '.jar', COPY '.jar' IN '/opt/clientesapp'
 ## FOLDER AND DELETE '.encrypt' AND '-tar' FILES AT '/temp' FOLDER
 cd /tmp
 tar -xf $SW_VERSION.tar -C /opt/clientesapp
-rm -rf /tmp/$SW_VERSION.encrypt
-rm -rf /tmp/$SW_VERSION.tar
+sleep 4
+cd /tmp
+rm -rf $SW_VERSION.encrypt
+sleep 1
+cd /tmp
+rm -rf $SW_VERSION.tar
+sleep 1
 
 ## SUCCESS MESSAGE
 echo "Desencriptación realizada con éxito, ejecutando $SW_VERSION.jar"
