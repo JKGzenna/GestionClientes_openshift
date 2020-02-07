32d5da645177fca3d7b6ee4bc27dc444c3120feb
JKGzenna <juanlu360@gmail.com>
2020-02-07 04:13:40 +0100

    Update entrypoint.sh

--- 1.0-encrypt/entrypoint.sh
+++ 1.0-encrypt/entrypoint.sh
@@ -5,15 +5,13 @@ encrypt_key=$(cat /etc/secret-volume/password)
 
 ## UNTAR THE ENCRYPT SW_VERSION, DELETE '*.tar'
 ## AND DECRYPT FILE TO '.tar' WITH OPENSSL.
-cd /tmp
-tar -xvf $SW_VERSION.tar --wildcards 'spring-boot-jpa-1.0.encrypt'
+tar -xf $SW_VERSION.tar --wildcards '*.encrypt'
 rm -rf $SW_VERSION.tar
-echo -n "$encrypt_key" | openssl enc -d -aes-256-cbc -in "/tmp/$SW_VERSION.encrypt" -out "/tmp/$SW_VERSION.tar" -pass stdin
+echo -n "$encrypt_key" | openssl enc -d -aes-256-cbc -in "$SW_VERSION.encrypt" -out "$SW_VERSION.tar" -pass stdin
 
 ## UNTAR FILE OF SW_VERSION TO '.jar', COPY '.jar' IN '/opt/clientesapp'
 ## FOLDER AND DELETE '.encrypt' AND '-tar' FILES AT '/temp' FOLDER
-cd /tmp
-tar -xvf $SW_VERSION.tar --wildcards 'spring-boot-jpa-1.0.jar'
+tar -xf $SW_VERSION.tar --wildcards 'spring-boot-jpa-1.0.jar'
 mv $SW_VERSION.jar /opt/clientesapp
 rm -rf $SW_VERSION.encrypt
 rm -rf $SW_VERSION.tar
