cb9798183980c88c24485222a21ad773ccef67fc
JKGzenna <juanlu360@gmail.com>
2020-02-07 03:15:16 +0100

    Update entrypoint.sh

--- 1.0-encrypt/entrypoint.sh
+++ 1.0-encrypt/entrypoint.sh
@@ -6,20 +6,20 @@ encrypt_key=$(cat /etc/secret-volume/password)
 ## UNTAR THE ENCRYPT SW_VERSION, DELETE '.tar'
 ## AND DECRYPT FILE TO '.tar' WITH OPENSSL
 cd /tmp
-tar --extract -vv --occurrence --file=./tmp/$SW_VERSION.tar $SW_VERSION.encrypt
+tar -xf $SW_VERSION.tar
 rm -rf /tmp/$SW_VERSION.tar
 echo -n "$encrypt_key" | openssl enc -d -aes-256-cbc -in "/tmp/$SW_VERSION.encrypt" -out "/tmp/$SW_VERSION.tar" -pass stdin
 
 ## UNTAR FILE OF SW_VERSION TO '.jar', COPY '.jar' IN '/opt/clientesapp'
 ## FOLDER AND DELETE '.encrypt' AND '-tar' FILES AT '/temp' FOLDER
 cd /tmp
-tar --extract -vv --occurrence --file=./tmp/$SW_VERSION.tar $SW_VERSION.jar -C /opt/clientesapp
-#tar -xf /tmp/$SW_VERSION.tar -C /opt/clientesapp --strip 1
+tar -xf $SW_VERSION.tar -C /opt/clientesapp
 rm -rf /tmp/$SW_VERSION.encrypt
 rm -rf /tmp/$SW_VERSION.tar
 
 ## SUCCESS MESSAGE
 echo "Desencriptación realizada con éxito, ejecutando $SW_VERSION.jar"
+sleep 3
 
 ## GO TO '/opt/clienteapp' FOLDER AND START '.jar'
 cd /opt/clientesapp
