cb26adc2f62f72261b0688eb0793eac1a45cc19f
JKGzenna <juanlu360@gmail.com>
2020-02-09 00:23:02 +0100

    update osh securized encrypt configuration
    update osh securized encrypt configuration

--- 1.0-encrypt/entrypoint.sh
+++ 1.0-encrypt/entrypoint.sh
@@ -6,7 +6,7 @@ encrypt_key=$(cat /etc/secret-volume/password)
 ## UNZIP THE ENCRYPT SW_VERSION, DELETE '*.tgz'
 ## AND DECRYPT FILE TO '.tar' WITH OPENSSL.
 echo "###  A continuación se muestra un error normal dado que al descomprimir no encuentra ningun directorio y encuentra un archivo encriptado  ###"
-tar -xz /tmp/$SW_VERSION.tgz /tmp
+tar -xzf /tmp/$SW_VERSION.tgz --wildcards 'spring-boot-jpa-1.0.encrypt'
 sleep 2
 rm -rf $SW_VERSION.tgz
 echo -n "$encrypt_key" | openssl enc -d -aes-256-cbc -in "$SW_VERSION.encrypt" -out "$SW_VERSION.tar" -pass stdin
@@ -17,6 +17,7 @@ tar -xf $SW_VERSION.tar --wildcards 'spring-boot-jpa-1.0.jar'
 mv $SW_VERSION.jar /opt/clientesapp
 rm -rf $SW_VERSION.encrypt
 rm -rf $SW_VERSION.tar
+echo ""
 
 ## SUCCESS MESSAGE
 echo "###  Desencriptación realizada con éxito, ejecutando $SW_VERSION.jar  ###"
