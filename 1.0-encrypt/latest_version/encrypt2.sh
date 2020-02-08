8eca576a76fa226ef9149dbff9a15a80295e3095
JKGzenna <juanlu360@gmail.com>
2020-02-09 00:49:59 +0100

    update osh securized encrypt configuration
    update osh securized encrypt configuration

--- 1.0-encrypt/latest_version/encrypt2.sh
+++ 1.0-encrypt/latest_version/encrypt2.sh
@@ -8,7 +8,7 @@ SW_VERSION="spring-boot-jpa-1.0"
 
 ## DAMOS PERMISOS AL '.jar'
 chmod +x $SW_VERSION.jar
-chmod 775 $SW_VERSION.jar
+chmod 777 $SW_VERSION.jar
 
 ## ENCRIPTAMOS EL '.jar' CON OPENSSL Y LO BORRAMOS
 echo -n "$encrypt_key" | openssl enc -e -aes-256-cbc -in "$SW_VERSION.jar" -out "$SW_VERSION.encrypt" -pass stdin
@@ -16,7 +16,7 @@ rm -rf $SW_VERSION.jar
 
 ## DAMOS PERMISOS AL '.encrypt'
 chmod +x $SW_VERSION.encrypt
-chmod 775 $SW_VERSION.encrypt
+chmod 777 $SW_VERSION.encrypt
 
 ##MENSAJE Y SALIDA
 echo "Encriptación realizada con éxito"
