1d29e300ab49bfe3f93a03e72905dda6fda491c5
JKGzenna <juanlu360@gmail.com>
2020-02-08 23:46:02 +0100

    update osh securized encrypt configuration
    update osh securized encrypt configuration

--- 1.0-encrypt/latest_version/decrypt.sh
+++ 1.0-encrypt/latest_version/decrypt.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-#COLOCAR EL '.encrypt' EN LA MISMA RUTA DEL SCRIPT Y EJECUTARLO DESDE ESE DIRECTORIO
+## COLOCAR EL '.encrypt' EN LA MISMA RUTA DEL SCRIPT Y EJECUTARLO DESDE ESE DIRECTORIO
 
-#CREAMOS LAS VARIABLES
+## CREAMOS LAS VARIABLES
 encrypt_key=$(cat /etc/secret-volume/password)
 SW_VERSION="spring-boot-jpa-1.0"
 
-#HACEMOS LAS CONVERSIONES, DESENCRIPTAMOS Y BORRAMOS LOS TEMPORALES GENERADOS
+## DESCOMPRIMIMOS EL '.encrypt' QUE HAY DENTRO DEL '.tgz' Y BORRAMOS EL '.tgz'
 tar -xzf $SW_VERSION.tgz
 rm -rf $SW_VERSION.tgz
+
+## DESENCRIPTAMOS CON OPENSSL Y BORRAMOS EL '.encrypt'
 echo -n "$encrypt_key" | openssl enc -d -aes-256-cbc -in "$SW_VERSION.encrypt" -out "$SW_VERSION.tar" -pass stdin
 rm -rf $SW_VERSION.encrypt
+
+## DESEMPAQUETAMOS EL '.jar' QUE HAY DENTRO DEL '.tar' Y BORRAMOS EL '.tar'
 tar -xf $SW_VERSION.tar
 rm -rf $SW_VERSION.tar
 
--- 1.0-encrypt/latest_version/encrypt.sh
+++ 1.0-encrypt/latest_version/encrypt.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
-#COLOCAR EL '.jar' EN LA MISMA RUTA DEL SCRIPT Y EJECUTARLO DESDE ESE DIRECTORIO
+## COLOCAR EL '.jar' EN LA MISMA RUTA DEL SCRIPT Y EJECUTARLO DESDE ESE DIRECTORIO
 
-#CREAMOS LAS VARIABLES
+## CREAMOS LAS VARIABLES
 encrypt_key=$(cat /etc/secret-volume/password)
 SW_VERSION="spring-boot-jpa-1.0"
 
-#DAMOS PERMISOS, HACEMOS LAS CONVERSIONES, ENCRIPTAMOS, DAMOS PERMISOS Y BORRAMOS LOS TEMPORALES GENERADOS
+## DAMOS PERMISOS AL '.jar'
 chmod +x $SW_VERSION.jar
-chmod 777 $SW_VERSION.jar
+chmod 775 $SW_VERSION.jar
+
+## EMPAQUETAMOS EN '.tar' Y BORRAMOS EL '.jar'
 tar -cf $SW_VERSION.tar $SW_VERSION.jar
 rm -rf $SW_VERSION.jar
+
+## ENCRIPTAMOS EL '.tar' CON OPENSSL Y BORRAMOS EL '.tar'
 echo -n "$encrypt_key" | openssl enc -e -aes-256-cbc -in "$SW_VERSION.tar" -out "$SW_VERSION.encrypt" -pass stdin
 rm -rf $SW_VERSION.tar
+
+## COMPRIMIMOS EN 'tgz' EL '.encrypt' Y BORRAMOS EL '.encrypt'
 tar -czf $SW_VERSION.tgz $SW_VERSION.encrypt
-chmod +x $SW_VERSION.tgz
-chmod 777 $SW_VERSION.tgz
 rm -rf $SW_VERSION.encrypt
 
-#MENSAJE Y SALIDA
+## DAMOS PERMISOS AL '.tgz'
+chmod +x $SW_VERSION.tgz
+chmod 775 $SW_VERSION.tgz
+
+##MENSAJE Y SALIDA
 echo "Encriptación realizada con éxito"
 exit
