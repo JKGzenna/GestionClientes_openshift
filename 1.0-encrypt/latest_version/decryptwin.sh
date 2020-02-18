9a68b81b8f9c03be20b5e35d492bee61d0d676d0
JKGzenna <juanlu360@gmail.com>
2020-02-18 01:16:15 +0100

    update osh securized encrypt configuration - win
    apt-get install openssl in mobaxterm 20.0
    use flag '-md md5' for old openssl versions

--- 1.0-encrypt/latest_version/decryptwin.sh
+++ 1.0-encrypt/latest_version/decryptwin.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 ## COLOCAR EL '.encrypt' EN LA MISMA RUTA DEL SCRIPT Y EJECUTARLO DESDE ESE DIRECTORIO
 ## DESENCRIPTAMOS EL '.encrypt' CON OPENSSL
+echo "Instale OpenSSL en Windows, en caso de no tener permisos de Adminsitrador utilice el terminal bash de MobaXterm ejecutando dentro de él 'apt-get install openssl -y' antes de usar este script, recuerde además en 'settings/general' del MobaXterm asignar una ruta para el 'Persistent Home Directory' y para el 'Persistent Root Directory' ;-)"
 openssl enc -d -aes-256-cbc -md md5 -in "spring-boot-jpa-1.0.encrypt" -out "spring-boot-jpa-1.0.jar"
